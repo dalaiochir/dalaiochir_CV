249fde424308efeb42e85bff1b11c7064793b1a4
dalaiochir <b.daagii111@gmail.com>
2025-12-04 20:38:41 +0700

    hhe

--- components/FloatingIcons.js
+++ components/FloatingIcons.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import icon1 from '../public/icon1.png';
-import icon2 from '../public/icon2.png';
+// import React from 'react';
+// import icon1 from '../public/icon1.png';
+// import icon2 from '../public/icon2.png';
 
-const FloatingIcons = () => {
-  return (
-    <>
-      <img src={icon1} alt="icon1" className="floating-icon" style={{ top: '10%', left: '5%' }} />
-      <img src={icon2} alt="icon2" className="floating-icon" style={{ top: '50%', left: '80%' }} />
-    </>
-  );
-};
+// const FloatingIcons = () => {
+//   return (
+//     <>
+//       <img src={icon1} alt="icon1" className="floating-icon" style={{ top: '10%', left: '5%' }} />
+//       <img src={icon2} alt="icon2" className="floating-icon" style={{ top: '50%', left: '80%' }} />
+//     </>
+//   );
+// };
 
+// export default FloatingIcons;
+
+// Temporary disabled to prevent build errors
+const FloatingIcons = () => null;
 export default FloatingIcons;
--- components/ParticlesBackground.js
+++ components/ParticlesBackground.js
@@ -1,46 +1,22 @@
-import React from 'react';
-import { loadFull } from "tsparticles";
-import Particles from "react-tsparticles";
+// import React from 'react';
+// import { loadFull } from "tsparticles";
+// import Particles from "react-tsparticles";
 
-const ParticlesBackground = () => {
-  const particlesInit = async (main) => {
-    await loadFull(main);
-  };
+// const ParticlesBackground = () => {
+//   const particlesInit = async (main) => { await loadFull(main); };
+//   const particlesLoaded = (container) => {};
+//   return (
+//     <Particles
+//       id="tsparticles"
+//       init={particlesInit}
+//       loaded={particlesLoaded}
+//       options={{ /* particle options */ }}
+//     />
+//   );
+// };
 
-  const particlesLoaded = (container) => {};
+// export default ParticlesBackground;
 
-  return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        background: { color: "#f9f9f9" },
-        fpsLimit: 60,
-        interactivity: {
-          events: {
-            onHover: { enable: true, mode: "repulse" },
-            onClick: { enable: true, mode: "push" },
-          },
-          modes: {
-            repulse: { distance: 100, duration: 0.4 },
-            push: { quantity: 4 },
-          },
-        },
-        particles: {
-          color: { value: "#ff6600" },
-          links: { enable: true, color: "#ff6600", distance: 150 },
-          collisions: { enable: false },
-          move: { enable: true, speed: 2 },
-          number: { density: { enable: true, area: 800 }, value: 40 },
-          opacity: { value: 0.5 },
-          shape: { type: "circle" },
-          size: { value: { min: 2, max: 5 } },
-        },
-        detectRetina: true,
-      }}
-    />
-  );
-};
-
+// Temporary disabled to prevent build errors
+const ParticlesBackground = () => null;
 export default ParticlesBackground;
